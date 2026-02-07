26c1f7d9b980e3e36d8433d4fba89d43a0112267
coderyu5523 <coderyu5523@naver.com>
2026-02-08 02:17:14 +0900

    step4 : 프로젝트 기능 구현

--- order/src/main/java/com/metacoding/order/orders/OrderItemRepository.java
+++ order/src/main/java/com/metacoding/order/orders/OrderItemRepository.java
@@ -1,6 +1,8 @@
 package com.metacoding.order.orders;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface OrderItemRepository extends JpaRepository<OrderItem, Integer> {
+    Optional<List<OrderItem>> findByOrderId(int orderId);
 }
