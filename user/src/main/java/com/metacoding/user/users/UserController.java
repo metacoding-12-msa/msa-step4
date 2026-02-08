7fb045a7886cac74ab79c00876ad2ced31a26a3d
coderyu <153602328+coderyu5523@users.noreply.github.com>
2026-02-08 11:44:20 +0900

    step4 : 프로젝트 기능 구현

--- user/src/main/java/com/metacoding/user/users/UserController.java
+++ user/src/main/java/com/metacoding/user/users/UserController.java
@@ -10,12 +10,12 @@ import com.metacoding.user.core.util.Resp;
 public class UserController {
     private final UserService userService;
 
-    @GetMapping("/api/users/{userId}")
+    @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody UserRequest requestDTO) {
         return Resp.ok(userService.login(requestDTO.username(), requestDTO.password()));
     }
 
-    @GetMapping("/{userId}")
+    @GetMapping("/api/users/{userId}")
     public ResponseEntity<?> getUser(@PathVariable("userId") int userId) {
         return Resp.ok(userService.findById(userId));
     }
